b60af6f5bc76ab28cba1bba6f393cdb228fa1020
HailanXyouknow <hailan@ualberta.ca>
2021-02-01 19:34:01 -0700

    answered questions

--- polls/urls.py
+++ polls/urls.py
@@ -6,9 +6,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'), # http://127.0.0.1:8000/polls/ + ''
-    path('<int:question_id>/', views.detail, name='detail'), # .../polls/5/
-    path('<int:question_id>/results/', views.results, name='results'), # .../polls/5/results/
-    path('<int:question_id>/vote/', views.vote, name='vote'),  # ex: .../polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
